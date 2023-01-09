18d1a669a3602df6aade13ffeecae819338723b9
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2023-01-09 21:22:33 +0300

    Update ArticleThree.vue

--- src/views/ArticleThree.vue
+++ src/views/ArticleThree.vue
@@ -4,55 +4,63 @@
       <div class="row">
         <p>{{ currentPar }}</p>
       </div>
+
+      <div>
+        Selected: {{ selected }} and current Group {{ userGroup }} and current
+        page {{ cuurentPage }}
+      </div>
       <div v-show="showQ" ref="box1" id="#box1" class="row">
-        <div class="form-group">
-          <p class="pt-0" id="left">{{ questions[cuurentPage].q }}</p>
-          <div id="left">
-            <input
-              class="form-check-input"
-              type="radio"
-              v-model="questionOneValue"
-              value="A"
-            />
-            <label class="form-check-label" for="2A">
-              {{ questions[cuurentPage].A }}
-            </label>
-          </div>
-          <div id="left">
-            <input
-              class="form-check-input"
-              type="radio"
-              v-model="questionOneValue"
-              value="B"
-            />
-            <label class="form-check-label" for="2B">
-              {{ questions[cuurentPage].B }}
-            </label>
-          </div>
-          <div id="left">
-            <input
-              class="form-check-input"
-              type="radio"
-              v-model="questionOneValue"
-              value="C"
-            />
-            <label class="form-check-label" for="2C">
-              {{ questions[cuurentPage].C }}</label
-            >
-          </div>
-          <div id="left">
-            <input
-              class="form-check-input"
-              type="radio"
-              v-model="questionOneValue"
-              value="D"
-            />
-            <label class="form-check-label" for="2D">
-              {{ questions[cuurentPage].D }}
-            </label>
+        <p id="item">{{ questions[cuurentPage].q }}</p>
+        <div class="col-6 offset-2">
+          <div class="form-group form-check">
+            <div id="left">
+              <input
+                class="form-check-input"
+                type="radio"
+                v-model="selected"
+                value="A"
+              />
+              <label class="form-check-label" for="2A">
+                {{ questions[cuurentPage].A }}
+              </label>
+            </div>
+            <div id="left">
+              <input
+                class="form-check-input"
+                type="radio"
+                v-model="selected"
+                value="B"
+              />
+              <label class="form-check-label" for="2B">
+                {{ questions[cuurentPage].B }}
+              </label>
+            </div>
+            <div id="left">
+              <input
+                class="form-check-input"
+                type="radio"
+                v-model="selected"
+                value="C"
+              />
+              <label class="form-check-label" for="2C">
+                {{ questions[cuurentPage].C }}</label
+              >
+            </div>
+            <div id="left">
+              <input
+                class="form-check-input"
+                type="radio"
+                v-model="selected"
+                value="D"
+              />
+              <label class="form-check-label" for="2D">
+                {{ questions[cuurentPage].D }}
+              </label>
+            </div>
           </div>
         </div>
       </div>
+
       <div class="row">
         <div class="mt-4">
           <button
@@ -84,7 +92,7 @@ export default {
   data() {
     return {
       readingSpeed: this.$store.state.readingSpeed,
-      userGroup: "A",
+      userGroup: "C",
       paragraphs: [
         {
           id: 0,
@@ -108,10 +116,7 @@ export default {
             "[MODIFED] The impact of poor readability on the success of content is significant, and that is becoming more apparent as the river of       content we all experience grows into more of a torrent. Attention   spans are decreasing. Distractions are increasing. The 280-character    tweet is taking over from the 2000 word article. With all that in   mind, it's incredibly important that, in addition to writing    interesting and engaging content, you write it in a way that people    find easy to take in. So people are turning to software, not as a   replacement for editorial expertise, but as a complement to it. A          computer can quickly use algorithms like the Flesch Reading Ease or          the Gunning Fog index to spot problematic passages of text. And that's          where we come in. At Readable.io, we are fascinated - almost obsessed      - with readability. We are always looking at more ways to measure it,     more ways to recommend improvements, and more ways to help our          customers make readability scoring and analysis a core part of their         content management strategies. Our software can analyse anything you       can throw at it, from Word documents to entire websites, and help you  focus your efforts where they are going to have the maximum impact",
         },
       ],
-      questionOneValue: null,
-      questionTwoValue: null,
-      questionThreeValue: null,
-
+      selected: "",
       questions: [
         {
           id: 0,
@@ -120,7 +125,7 @@ export default {
           B: "Option B",
           C: "Option C",
           D: "Option D",
-          cuurentAnswer: "C",
+          correctAnswer: "C",
         },
         {
           id: 1,
@@ -129,7 +134,7 @@ export default {
           B: "Option B",
           C: "Option C",
           D: "Option D",
-          cuurentAnswer: "C",
+          correctAnswer: "C",
         },
         {
           id: 2,
@@ -167,11 +172,17 @@ export default {
     start() {
       this.startReadingTime = Date.now();
       console.log(this.readingSpeed);
-      const par = this.paragraphs[0].orginalText;
-      this.currentPar = par;
       this.showStartBtn = false;
       this.showNextBtn = true;
       this.showQ = true;
+      this.cuurentPage++;
+      if (this.userGroup === "B") {
+        const par = this.paragraphs[this.cuurentPage].easyText;
+        this.currentPar = par;
+      } else {
+        const par = this.paragraphs[this.cuurentPage].orginalText;
+        this.currentPar = par;
+      }
     },
     next() {
       if (this.cuurentPage < this.paragraphs.length) {
@@ -186,7 +197,7 @@ export default {
           this.currentPar = par;
           this.cuurentPage++;
         } else {
-          if (this.questions[this.cuurentPage].cuurentAnswer != "C") {
+          if (this.questions[this.cuurentPage].correctAnswer != this.selected) {
             const par = this.paragraphs[this.cuurentPage].easyText;
             this.currentPar = par;
             this.cuurentPage++;
@@ -215,14 +226,30 @@ p {
 .mobileContainer {
   margin: 50px;
 }
-.footer {
-  margin-block: 1000px;
-  color: whitesmoke;
-}
 #myElement1,
 #myElement2,
 #myElement3 {
   margin: 10px;
   border: 2px solid black;
+}
+
+.form-check-label {
+  font-size: 18px;
+}
+
+#left {
+  text-align: left;
+}
+#right {
+  float: right;
+  margin: 40px 0px;
+}
+.form-group {
+  margin-bottom: 40px;
+}
+
+#item {
+  font-size: 18px;
+  font-weight: bold;
 }
 </style>
